00cd254b988db56262dde1ff81a47a085fdbba55
DehtyarAlona <alonavertical@gmail.com>
2016-03-15 11:49:12 -0400

    added File

---
+++ otherProjects/ajaxExamples/js/myClients.js
@@ -0,0 +1,18 @@
+$(document).ready(function (){
+
+
+
+$("#getClients").on("click", function() {
+
+var url ="http://DehtyarAlona.github.io/otherProjects/ajaxExamples/json/clients.json"
+$.getJSON(url, function (data){
+  $.each(data,function(index,item){
+    $("#data").append(item.name);
+
+
+  })
+//alert(data);
+//console.dir(data);
+})
+
+})
