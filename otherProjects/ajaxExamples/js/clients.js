4c4e1da05aade8e07e206c0b428e88d0796d12be
DehtyarAlona <alonavertical@gmail.com>
2016-03-15 11:41:53 -0400

    Updated Data

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -6,8 +6,13 @@ $("#getClients").on("click", function() {
 
 var url ="http://DehtyarAlona.github.io/otherProjects/ajaxExamples/json/clients.json"
 $.getJSON(url, function (data){
-alert(data);
-console.dir(data);
+  $.each(data,function(index,item){
+    $("#data").append(item.name);
+
+
+  })
+//alert(data);
+//console.dir(data);
 })
 
 })
