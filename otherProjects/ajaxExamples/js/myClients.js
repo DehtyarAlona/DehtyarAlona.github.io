575560c8ebe9719af3e8c8dd3051e6f428572f54
DehtyarAlona <alonavertical@gmail.com>
2016-03-29 15:18:45 -0400

    added functions

--- otherProjects/ajaxExamples/js/myClients.js
+++ otherProjects/ajaxExamples/js/myClients.js
@@ -1,19 +1,30 @@
-$(document).ready(function (){
+$(document).ready(function() {
 
+    $("#getClients").on("click", function() {
 
+        var url = "http://DehtyarAlona.github.io/otherProjects/ajaxExamples/json/clients.json";
 
-$("#getClients").on("click", function() {
+        $.getJSON(url, function(data) {
+            var html = "<table class='table table-hover table-striped'>" +
+              "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
-var url ="http://DehtyarAlona.github.io/otherProjects/ajaxExamples/json/clients.json"
-$.getJSON(url, function (data){
-  $.each(data,function(index,item){
-    $("#data").append(item.name);
+            $.each(data, function(index, item) {
 
+              html += "<tr>" +
+                "<td>" + item.name + "</td>" +
+                "<td>" + item.email + "</td>" +
+                "<td>" + item.company + "</td>" +
+                "</tr>";
+            })
 
-  })
-//alert(data);
-//console.dir(data);
-})
+            html += "</table>";
+            $("#data").append(html);
 
-})
-})
+            //alert(data);
+            //console.dir(data);
+
+          }) //getJSON
+
+      }) //click
+
+  }) //ready
